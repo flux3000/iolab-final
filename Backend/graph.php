affddcc89b5b9afd567ef4979f4172be5aa6d5d3
tomnar <tomquast89@gmail.com>
2013-11-27 02:09:43 -0800

    sample data for backend

---
+++ Backend/graph.php
@@ -0,0 +1,42 @@
+[
+{
+	"year":"1999", 
+	"month":"10", 
+	"sightings": "13423",
+	"movies":[
+		{
+			"id" : 123,
+			"title" : "trololol the movie",
+			"image" : "http://ia.media-imdb.com/images/M/MV5BMTcxNDE1OTgyOF5BMl5BanBnXkFtZTcwMTEyMzMxOQ@@._V1_SX300.jpg",
+			"ATA" : "45789"
+		},
+		{
+			"id" : 124,
+			"title" : "haha the movie",
+			"image" : "http://ia.media-imdb.com/images/M/MV5BMjI0NDI1MTMyM15BMl5BanBnXkFtZTcwMDMzMTcyNA@@._V1_SX300.jpg",
+			"ATA" : "345"
+		},
+		{
+			"id" : 125,
+			"title" : "test mov",
+			"image" : "http://ia.media-imdb.com/images/M/MV5BMTk2NzczOTgxNF5BMl5BanBnXkFtZTcwODQ5ODczOQ@@._V1_SX300.jpg",
+			"ATA" : "2234"
+		}
+	]
+	
+},
+{
+	"year":"1999", 
+	"month":"11", 
+	"sightings": "1323",
+	"movies":[
+		{
+			"id" : 125,
+			"title" : "trololol the movie",
+			"image" : "http://ia.media-imdb.com/images/M/MV5BMTcxNDE1OTgyOF5BMl5BanBnXkFtZTcwMTEyMzMxOQ@@._V1_SX300.jpg",
+			"ATA" : "4"
+		}
+	]
+	
+}
+]
---
+++ Backend/mapper.php
@@ -0,0 +1,20 @@
+{
+"total" : "123234",
+"fyear" : "1980",
+"fmonth" : "10",
+"tyear" : "1999",
+"tmonth" : "10",
+"cities":[
+	{
+		"city": "San Francisco",
+		"sightings": "345"
+	},
+	{
+		"city": "Berkeley",
+		"sightings": "234"
+	},
+	{
+		"city": "New York",
+		"sightings": "2"
+	}]
+}
