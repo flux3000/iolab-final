5dfcb069169ef960444e05491b7c67122b5f9d55
tomnar <tomquast89@gmail.com>
2013-11-28 01:04:13 -0800

    added db to parsing

--- Backend/scraper.php
+++ Backend/scraper.php
@@ -1,13 +1,16 @@
 <?php
 	include_once 'simple_html_dom.php';
 	
+	$mysqli = new mysqli("localhost", "root", "", "iolab_ass5");
+	if($mysqli->connect_errno){printf("Connect failed: %s\n", $mysqli->connect_error);exit();}
+	
 	$html = file_get_html('http://www.nuforc.org/webreports/ndxevent.html');
 	
 	// Find all images
 	foreach($html->find('table') as $main){ //find the main table
 		$links = $main->find('a'); //get all the links to the subpages
-		//for ($i = 0; $i < sizeof($links)-1; $i++){ //loop through them (and ignore the last one which has NO DATE
-		for ($i = 0; $i < 1; $i++){
+		for ($i = 3; $i < sizeof($links)-1; $i++){ //loop through them (and ignore the last one which has NO DATE
+		//for ($i = 0; $i < 1; $i++){
 			$link = $links[$i]->href;
 			$subpage = getRowsOfSubpage('http://www.nuforc.org/webreports/'.$link);	
 		}
@@ -15,16 +18,23 @@
 	
 	function getRowsOfSubpage($link){
 		$subpage = file_get_html($link); //get content of subpage
-		foreach($subpage->find('table') as $main){ //find the main table
-			$rows = $main->find('tr'); //get all the rows holding the reports
-			//for ($i = 1; $i < sizeof($rows); $i++){ //loop through them (Ignore the first element
-			for ($i = 1; $i < 2; $i++){ //ignore the first element
-				getContentOfRow($rows[$i]);
+		if(!$subpage){
+				echo 'ERROR: link'; //too much invalid html, the page can't be parsed
+		}
+		else{
+			foreach($subpage->find('table') as $main){ //find the main table
+				$rows = $main->find('tr'); //get all the rows holding the reports
+				for ($i = 1; $i < sizeof($rows); $i++){ //loop through them (Ignore the first element
+				//for ($i = 1; $i < 2; $i++){ //ignore the first element
+					getContentOfRow($rows[$i]);
+				}
 			}
 		}
 	}
 	
 	function getContentOfRow($row){
+		global $mysqli;
+		
 		$fields = $row->find('td'); //find all fields (Date/Time|City|State|Shape|Duration|Summary|Posted)
 		$date = $fields[0]->plaintext;
 		$link = 'http://www.nuforc.org/webreports/' . $fields[0]->find('a')[0]->href;
@@ -36,12 +46,10 @@
 		$post_date = $fields[6]->plaintext;
 		
 		echo "date: " . $date . '<br/>';
-		echo "link: " . $link . '<br/>';
-		echo "city: " . $city . '<br/>';
-		echo "state: " . $state . '<br/>';
-		echo "shape: " . $shape . '<br/>';
-		echo "duration: " . $duration . '<br/>';
-		echo "summary: " . $summary . '<br/>';
-		echo "post date: " . $post_date . '<br/>';
+		
+		$stmt = $mysqli->prepare("INSERT INTO ufos (date, url, city, state, shape, duration, summary, post_date) VALUES (?,?,?,?,?,?,?,?)");
+		$stmt->bind_param("ssssssss", $date, $link, $city, $state, $shape, $duration, $summary, $post_date);
+		$stmt->execute();
+		
 	}
 ?>
