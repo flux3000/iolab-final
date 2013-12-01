f49edf3aadb3f0240cfd8cc02835408b3654be7e
tomnar <tomquast89@gmail.com>
2013-11-30 17:16:04 -0800

    added real JSON output

--- Backend/graph.php
+++ Backend/graph.php
@@ -1,42 +1,35 @@
-[
-{
-	"year":"1999", 
-	"month":"10", 
-	"sightings": "13423",
-	"movies":[
-		{
-			"id" : 123,
-			"title" : "trololol the movie",
-			"image" : "http://ia.media-imdb.com/images/M/MV5BMTcxNDE1OTgyOF5BMl5BanBnXkFtZTcwMTEyMzMxOQ@@._V1_SX300.jpg",
-			"ATA" : "45789"
-		},
-		{
-			"id" : 124,
-			"title" : "haha the movie",
-			"image" : "http://ia.media-imdb.com/images/M/MV5BMjI0NDI1MTMyM15BMl5BanBnXkFtZTcwMDMzMTcyNA@@._V1_SX300.jpg",
-			"ATA" : "345"
-		},
-		{
-			"id" : 125,
-			"title" : "test mov",
-			"image" : "http://ia.media-imdb.com/images/M/MV5BMTk2NzczOTgxNF5BMl5BanBnXkFtZTcwODQ5ODczOQ@@._V1_SX300.jpg",
-			"ATA" : "2234"
-		}
-	]
+<?php 
+//Other interesting data:
+//SELECT `date`, COUNT(`date`) FROM `ufos` GROUP BY MONTH(`date`)
+//SELECT `date`, COUNT(`date`) FROM `ufos` GROUP BY YEAR(`date`)
+
+$mysqli = new mysqli("localhost", "root", "", "iolab_ass5");
+if($mysqli->connect_errno){printf("Connect failed: %s\n", $mysqli->connect_error);exit();}
+
+$stmt = $mysqli->prepare("SELECT DATE_FORMAT(`date`, '%Y %m') AS DATUM, COUNT(*) AS SIGHTINGS FROM `ufos` GROUP BY DATE_FORMAT(`date`, '%Y %m')");
+$stmt->execute();
+$stmt->bind_result($date, $sightings);
+
+$year = "-1";
+$res = '[';
+while ($stmt->fetch()) {
+	$date = explode(" ", $date);
+	$elYear = $date[0];
+	$elMonth = $date[1];
 	
-},
-{
-	"year":"1999", 
-	"month":"11", 
-	"sightings": "1323",
-	"movies":[
-		{
-			"id" : 125,
-			"title" : "trololol the movie",
-			"image" : "http://ia.media-imdb.com/images/M/MV5BMTcxNDE1OTgyOF5BMl5BanBnXkFtZTcwMTEyMzMxOQ@@._V1_SX300.jpg",
-			"ATA" : "4"
-		}
-	]
-	
+	if($year == "-1"){ //starting to build json, dont append stuff
+		$res .= '{"year":"' . $elYear . '", "months":[{ "month":"' . $elMonth . '", "sightings":"' . $sightings . '"';
+		$year = $elYear;
+	}
+	elseif($elYear != $year){ //a new year
+		$res .= '}]},{"year":"' . $elYear . '", "months":[{ "month":"' . $elMonth . '", "sightings":"' . $sightings . '"';
+		$year = $elYear;
+	}
+	else{ //a extra month in the same year
+		$res .= '}, {"month":"' . $elMonth . '", "sightings":"' . $sightings . '"';
+	}
 }
-]
+$res .= '}]}]';
+
+echo $res;
+?>
