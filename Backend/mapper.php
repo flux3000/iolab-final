3048f7758dda7c757a9ac036ed6921f7530311b2
tomnar <tomquast89@gmail.com>
2013-11-30 17:35:23 -0800

    added real JSON to the mapper

--- Backend/mapper.php
+++ Backend/mapper.php
@@ -1,20 +1,23 @@
-{
-"total" : "123234",
-"fyear" : "1980",
-"fmonth" : "10",
-"tyear" : "1999",
-"tmonth" : "10",
-"cities":[
-	{
-		"city": "San Francisco",
-		"sightings": "345"
-	},
-	{
-		"city": "Berkeley",
-		"sightings": "234"
-	},
-	{
-		"city": "New York",
-		"sightings": "2"
-	}]
+<?php 
+$mysqli = new mysqli("localhost", "root", "", "iolab_ass5");
+if($mysqli->connect_errno){printf("Connect failed: %s\n", $mysqli->connect_error);exit();}
+
+$stmt = $mysqli->prepare("SELECT `state`, COUNT(*) AS SIGHTINGS FROM `ufos` GROUP BY `state` ORDER BY COUNT(*) DESC");
+$stmt->execute();
+$stmt->bind_result($state, $sightings);
+
+$sState = "-1";
+$res = '[';
+while ($stmt->fetch()) {	
+	if($sState == "-1"){ //starting to build json, dont append stuff
+		$res .= '{"name":' . json_encode($state) . ', "sightings":' . json_encode($sightings);
+		$sState = $state;
+	}
+	else{ //a new year
+		$res .= '},{"name":' . json_encode($state) . ', "sightings":' . json_encode($sightings);
+	}
 }
+$res .= '}]';
+
+echo $res;
+?>
