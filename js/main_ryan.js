6a061d350c11491a57b46fee417a77e996e93bd0
flux3000 <ryanfbaker@gmail.com>
2013-12-06 22:33:43 -0800

    added smaller day-by-day viz in sidebar, fixed axes mostly

--- js/main_ryan.js
+++ js/main_ryan.js
@@ -75,7 +75,7 @@ function graphData(data){
 	var numPoints = data.length;
 	// set up the svg 	
 	var w = 1190;
-	var h = 300;
+	var h = 250;
 	var svg = d3.select("#visualization");
 	svg.attr("width", w).attr("height", h);
 
@@ -103,7 +103,7 @@ function graphData(data){
 	//xAxisScale and yAxisScale are for the axes
 	var xAxisScale = d3.time.scale()
 		.domain([startDate, endDate])
-		.range([0, w]);
+		.range([0, BTW*numPoints]);
 	
 	var yAxisScale = d3.scale.linear()
 		.domain([0, 1000])
@@ -168,7 +168,6 @@ function graphData(data){
 				return "#b34100";
 			},
 			"class" : "bar"
-		
 	    })
 		.on("click", function(d, i) {
 			// run when user clicks on a bar in the chart. 
@@ -181,6 +180,7 @@ function graphData(data){
 				success: function(data) {
 					displayDetailsHeader(d.year, d.month, d.sightings); 
 					displayDetails(data);
+					prepareMonthData(data, d.year, d.month);
 				},
 				error: function(e){console.log("error: " + e);}
 			});
@@ -189,7 +189,7 @@ function graphData(data){
 	//Changing color of the rect when clicked
 	$(".bar").click(function() {
 		$(this).siblings().attr("fill", "#b34100");
-		$(this).attr("fill", "#FFD573");
+		$(this).attr("fill", "#FFD573"); // Yellow highlight
 		//$( this ).toggleClass("selected", addOrRemove); // doesn't work, because fill attribute overrides classes
 	});
 
@@ -211,6 +211,17 @@ function graphData(data){
 			$(".info").hide();
 		}
 	);
+
+}
+
+function displayDetailsChart(data) {
+	var json = JSON.parse(data);
+	//console.log(json);
+		thisSightingDateArr = json[i]["date"].split("-");
+		thisSightingYear = thisSightingDateArr[0];
+		thisSightingMonth = thisSightingDateArr[1];
+		thisSightingDay = thisSightingDateArr[2];	
+	graphMonthData(json);
 
 }
 
@@ -237,12 +248,12 @@ function displayDetails(data){
 		thisSightingYear = thisSightingDateArr[0];
 		thisSightingMonth = thisSightingDateArr[1];
 		thisSightingDay = thisSightingDateArr[2];
-		var a = moment([thisSightingYear, thisSightingMonth-1, thisSightingDay-1])
+		var a = moment([thisSightingYear, thisSightingMonth-1, thisSightingDay])
 		var displayDate = a.format("D MMMM YYYY");
 
 		thisSightingHTML = "<div class='sighting-item'>";
 		thisSightingHTML += "<div class='title'>" + displayDate + " - " + json[i]["city"] + ", " + json[i]["state"] + "</div>";
-		thisSightingHTML += "<div class='description'><strong>Shape:</strong> " + json[i]["shape"] + "<br><strong>Duration:</strong> " + json[i]["duration"] + "<br><br>" + json[i]["summary"] + "</div>";
+		thisSightingHTML += "<div class='description'><strong>Shape:</strong> " + json[i]["shape"] + "&nbsp;&nbsp;<strong>Duration:</strong> " + json[i]["duration"] + "<br><br>" + json[i]["summary"] + "</div>";
 		thisSightingHTML += "</div>";
 
 		$("#sighting-list").append("<li>"+thisSightingHTML+"</li>");
@@ -251,10 +262,189 @@ function displayDetails(data){
 
 }
 
+function prepareMonthData(data, year, month){
+
+	// this creates the small chart in the left sidebar, displaying the day-by-day breakdown of reports in a chosen month.
+
+	myMonthUFOs = [];
+	var dayCount = 0;
+	var json = JSON.parse(data);
+
+	// go through each day in the calendar month. If there is a sighting for this day in our dataset, we will record the sightings under the "count" variable. If there is no data, "count" will be 0.
+
+	for (var j = 1; j < 32; j++){ // needs fix to figure out days in month
+		var thisPoint = [];
+		thisPoint.year = year;
+		thisPoint.month = month;
+		thisPoint.day = j;
+		thisPoint.sightings = 0; 
+		for (var i = 0; i < data.length; i++) {
+			//console.log(json[i]);
+			
+			if (typeof json[i] != 'undefined'){
+				
+				thisSightingDateArr = json[i]["date"].split("-");
+				thisSightingDay = thisSightingDateArr[2];
+
+				if (thisSightingDay == j) {
+					thisPoint.sightings += 1;
+				}				
+			}
+	    }
+	    console.log(thisPoint.year + "-" + thisPoint.month + "-"+thisPoint.day+" : " + thisPoint.sightings);
+
+		myMonthUFOs.push(thisPoint);    
+	}
+    graphMonthData(myMonthUFOs);
+}
+
+
+function graphMonthData(data){
+	console.log(data);
+
+	$("#month-visualization").empty();
+
+	var numPoints = data.length;
+	// set up the svg 	
+	var w = 350;
+	var h = 100;
+	var svg = d3.select("#month-visualization");
+	svg.attr("width", w).attr("height", h);
+
+	//setting variables for drawing
+	var BW = 9; //Bar width
+	var BTW = BW+2; //Bar Total Width
+	var OL = 24; //Offset Left
+
+	//Setting the scales for the graph
+	// xScale and yScale are for the data points
+	var xScale = d3.time.scale()
+		.domain([1, numPoints])
+		.range([0, BTW*numPoints]);
+
+	var yScale = d3.scale.linear()
+		.domain([0, d3.max(data, function(d) { return d.sightings; })])
+		.range([0, h-5]);
+
+	//console.log("startdate:"+startDate);
+	//console.log("enddate:"+endDate);
+
+	//xAxisScale and yAxisScale are for the axes
+	var xAxisScale = d3.scale.linear()
+		.domain([1, numPoints]) // need to fix - days in month
+		.range([0, BTW*numPoints]); // need to fix
+	
+	var yAxisScale = d3.scale.linear()
+		.domain([0, d3.max(data, function(d) { return d.sightings; })])
+		.range([h-5, 0]);
+
+	//Creating the y-axis
+	var yAxis = d3.svg.axis()
+        .scale(yAxisScale)
+        .orient("left")
+		.ticks(5);
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(14," + 0 + ")")
+		.call(yAxis);
+
+	//Creating the x-axis
+	var xAxis = d3.svg.axis()
+        .scale(xAxisScale)
+        .orient("bottom")
+        .tickSize(2, 0)
+        .ticks(10);
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(14," + (h-5) + ")")
+		.call(xAxis);
+	
+	//Drawing the bars of the graph
+	svg.selectAll("rect")
+		.data(data)
+		.enter()
+		.append("rect")	
+		.attr({
+			"width": BW,		
+			"x": function(d, i) {
+				return 15 + i*BTW;
+			},
+        	"height": 0,
+			"y": function(d, i) {
+				return h-5;
+			} ,   
+			"desc": function(d, i) {
+				var v = moment([d.year, d.month-1, d.day])
+				var displayDate = v.format("MMMM Do YYYY");
+				return "<div class='title'>"+displayDate+"</div><div class='description'><strong>"+d.sightings+"</strong></div>";
+
+			},
+			"fill" : function(d, i){
+				return "#FFD573"; // yellow
+			},
+			"stroke-width": 1,
+			"stroke": "#C99C30",
+			"class" : "smallbar",
+	    })
+        .transition()
+        .attr({
+        	"height": function(d, i) {
+			    return yScale(d.sightings);
+			},
+			"y": function(d, i) {
+				return h - 5 - yScale(d.sightings);
+			}       
+    	})
+        .duration(500)	
+
+/*		.on("click", function(d, i) {
+			// run when user clicks on a bar in the chart. 
+			//populate sidebar with details on this month's sightings.
+			var dataString = 'month='+d.month+'&year='+d.year;
+
+			$.ajax({
+				url: "http://ufo.quast.li/backend/ufoMapper.php",
+				data: dataString,
+				success: function(data) {
+					displayDetailsHeader(d.year, d.month, d.sightings); 
+					displayDetails(data);
+				},
+				error: function(e){console.log("error: " + e);}
+			});
+		});		    
+*/
+	//Changing color of the rect when clicked
+	$(".smallbar").click(function() {
+		$(this).siblings().attr("fill", "#FFD573");
+		$(this).attr("fill", "#b34100");
+		//$( this ).toggleClass("selected", addOrRemove); // doesn't work, because fill attribute overrides classes
+	});
+
+	//Animating the rects of the diagrams on hover
+	$(".smallbar").hover(
+		function() {
+			var y = parseFloat($(this).attr("y")) + 0;
+			$(this).css("opacity", "0.6").attr("y", y);
+			
+			//Setting the info-text
+			var txt = $(this).attr("desc");
+			var left = $(this).position().left - 40;
+			var top = h - 50;
+			$(".sidebar-info").html(txt).css({"left" : left, "top" : top}).show();
+		}, 
+		function() {
+			var y = parseFloat($(this).attr("y")) - 0;
+			$(this).css("opacity", "1").attr("y", y);
+			$(".sidebar-info").hide();
+		}
+	);
+
+}
+
+
 function mapData(data){
 	//console.log(data);
 }
-
 
 // google maps code
 var map;
