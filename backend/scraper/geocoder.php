60b2f450b32333385ae822f8f89ce0651ff9f2af
tomnar <tomquast89@gmail.com>
2013-12-10 20:09:13 -0800

    Added comments to scraper files

--- backend/scraper/geocoder.php
+++ backend/scraper/geocoder.php
@@ -1,7 +1,6 @@
 <?php
-//username sliaheac@wegwerfemail.de
-//passw sliaheac@wegwerfemail.de
-//api key Fmjtd%7Cluubn16rn0%2Crl%3Do5-90asl0 
+//NB: easy way to check if there are any entries without citites:
+//SQL: SELECT ufoCities.city, ufoCities.state FROM (SELECT * FROM ufos GROUP BY city) AS ufoCities LEFT OUTER JOIN cities on ufoCities.city = cities.name WHERE cities.lat IS NULL;
 
 require_once '../constants.php';
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
@@ -14,14 +13,14 @@ $stmt->bind_result($cityR, $stateR);
 
 $stateStr = "";
 $count = 1;
-$cityArray = [];
-$allCities = [];
+$cityArray = []; //create an array for bulk citie queries, up to 100 cities at a time
+$allCities = []; //create an array for holding all cities as they are described in the db (used for joining)
 
 while ($stmt->fetch()) {
 	if($count <= 100){
 		$count++;
-		$city = preg_replace('/[^a-zA-Z0-9.]/',"+",$cityR);
-		$state = preg_replace('/[^a-zA-Z0-9.]/',"+",$stateR);
+		$city = preg_replace('/[^a-zA-Z0-9.]/',"+",$cityR); //remove weird symbols
+		$state = preg_replace('/[^a-zA-Z0-9.]/',"+",$stateR); //remove weird symbols
 		
 		$stateStr .= "&location=" . $city . "," . $state;
 		array_push($allCities,$cityR);
@@ -35,22 +34,24 @@ while ($stmt->fetch()) {
 
 //look up all cities and save their geocode in the database. take 100 searches at a time from the prepared array.
 for($u = 0; $u < count($cityArray)-1; $u++){
-
+	
+	//Calling the api, keep in mind that only a few queries are allowed per day.
 	$key = "Fmjtd%7Cluubn1u7l1%2Cbw%3Do5-90bnlw";
 	$url = "http://www.mapquestapi.com/geocoding/v1/batch?key=".$key.$cityArray[$u]."&thumbMaps=false&maxResults=1&outFormat=csv";
 	
+	//Getting the data back
 	$res = file_get_contents($url);
 	
-	$cityCounter = 0;
- 	$resA = explode(",", $res);
+	$cityCounter = 0; //Used for finding the city in the $allCities array
+ 	$resA = explode(",", $res); //The data format is cvs, so explode it and iterate through it
  	for ( $i = 0; $i < sizeof($resA); $i++){
  		if($i % 15 == 0){ //country e.g. "Country"|"DisplayLng" "US"|"-122.329437" "US"|"-112.075768" "US"|"
  			if($i != 0 && $i < 1501){
- 				$city = trim($resA[$i + 3], '"'); //remove trailing "
+ 				$city = trim($resA[$i + 3], '"'); //remove trailing "'s
  				$lat = trim($resA[$i + 6], '"');
  				$lng = trim($resA[$i + 7], '"');
 	
-  				$stmt = $mysqli->prepare("INSERT INTO cities (name, lat, lng) VALUES (?,?,?)");
+  				$stmt = $mysqli->prepare("INSERT INTO cities (name, lat, lng) VALUES (?,?,?)"); //Add the new city to the DB
   				$stmt->bind_param("sdd", $allCities[$cityCounter+$u*100], $lat, $lng);
   				$stmt->execute();
  				$cityCounter++;
@@ -58,6 +59,7 @@ for($u = 0; $u < count($cityArray)-1; $u++){
  		}
  	}
 }
+echo 'done';
 
 
 ?>
--- backend/scraper/scraperAdd.php
+++ backend/scraper/scraperAdd.php
@@ -1,15 +1,19 @@
 <?php
+	// This file is used to add data to a existing database, see errors/example.php for more infor about the format
+	
 	include_once 'simple_html_dom.php';
 	require_once '../constants.php';
 	
 	$file = 'errors/xxx'; //enter file here
 	
+	// Connect to the database
 	$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 	if($mysqli->connect_errno){printf("Connect failed: %s\n", $mysqli->connect_error);exit();}
 	
+	// Get the content of the file
 	$html = file_get_html($file);
 	
-	// Find all images
+	// Find the main table
 	$rows = $html->find('tr'); //get all the rows holding the reports
 	for ($i = 0; $i < sizeof($rows); $i++){ //loop through them
 		getContentOfRow($rows[$i]);
@@ -29,6 +33,7 @@
 		$summary = $fields[5]->plaintext;
 		$post_date = $fields[6]->plaintext;
 		
+		// Insert the new element to the database
 		$stmt = $mysqli->prepare("INSERT INTO ufos (date, url, city, state, shape, duration, summary, post_date) VALUES (?,?,?,?,?,?,?,?)");
 		$stmt->bind_param("ssssssss", $date, $link, $city, $state, $shape, $duration, $summary, $post_date);
 		$stmt->execute();
